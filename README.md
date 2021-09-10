a7a705a5c7d5076779b223080b0bfd5ccf864f71
Zeke Gabrielse <ezekg@yahoo.com>
2021-09-10 14:46:04 -0500

    update to use ed25519 signatures

--- README.md
+++ README.md
@@ -12,16 +12,11 @@ Feel free to cache to another form of local storage, e.g. registry, etc.
 
 First up, configure a few environment variables:
 ```bash
-# Your Keygen account ID (find yours at https://app.keygen.sh/settings)
-export KEYGEN_ACCOUNT_ID="YOUR_KEYGEN_ACCOUNT_ID"
+# Your Keygen account's DER encoded Ed25519 verify key
+export KEYGEN_VERIFY_KEY="MCowBQYDK2VwAyEA6GAeSLaTg7pSAkX9B5cemD0G0ixCV8/YIwRgFHnO54g="
 
-# Your Keygen account's public key (used to verify signatures)
-export KEYGEN_PUBLIC_KEY=$(printf %b \
-  '-----BEGIN PUBLIC KEY-----\n' \
-  'zdL8BgMFM7p7+FGEGuH1I0KBaMcB/RZZSUu4yTBMu0pJw2EWzr3CrOOiXQI3+6bA\n' \
-  # …
-  'efK41Ml6OwZB3tchqGmpuAsCEwEAaQ==\n' \
-  '-----END PUBLIC KEY-----')
+# Your Keygen account ID (find yours at https://app.keygen.sh/settings)
+export KEYGEN_ACCOUNT_ID="1fddcec8-8dd3-4d8d-9b16-215cac0f9b52"
 ```
 
 You can either run each line above within your terminal session before
@@ -29,13 +24,16 @@ starting the app, or you can add the above contents to your `~/.bashrc`
 file and then run `source ~/.bashrc` after saving the file.
 
 Next, install dependencies with [`yarn`](https://yarnpkg.comg):
-```
+
+```bash
 yarn
 ```
 
 Then run the script and input a license key:
-```
+
+```bash
 yarn start
+# => Enter your license key: DEMO-AABCCD-7F6E4A-E64012-340C88-V3
 ```
 
 ## Questions?
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {
-  KEYGEN_PUBLIC_KEY,
+  KEYGEN_VERIFY_KEY,
   KEYGEN_ACCOUNT_ID
 } = process.env
 
@@ -7,22 +7,77 @@ const fetch = require('node-fetch')
 const readline = require('readline')
 const chalk = require('chalk')
 const crypto = require('crypto')
-const fs = require('fs')
+const fs = require('fs/promises')
 
 const rl = readline.createInterface(
   process.stdin,
   process.stdout
 )
 
+// Promp the user for their license key. Returns a license key string.
 async function getLicenseKeyFromUser() {
   return new Promise(resolve =>
     rl.question(chalk.yellow('Enter your license key: '), a => resolve(a))
   )
 }
 
+// There is likely a third-party module for this, but we want to show
+// how to parse the signature header without one. Returns an object.
+function parseParameterizedHeader(header) {
+  if (header == null) {
+    return null
+  }
+
+  const params = header.split(/,\s*/g)
+  const keyvalues = params.map(param => {
+    const [, key, value] = param.match(/([^=]+)="([^"]+)"/i)
+
+    return [key, value]
+  })
+
+  return keyvalues.reduce(
+    (o, [k, v]) => (o[k] = v, o),
+    {}
+  )
+}
+
+// Verify the signature of a response. Returns void. Throws if invalid.
+// See: https://keygen.sh/docs/api/#response-signatures
+async function verifyResponseSignature({ target, digest, date, signature }) {
+  if (signature == null) {
+    throw new Error('Signature was expected but is missing')
+  }
+
+  // Rebuild the signing data
+  const data = [
+    `(request-target): ${target}`,
+    `host: api.keygen.sh`,
+    `date: ${date}`,
+    `digest: ${digest}`,
+  ].join('\n')
+
+  // Decode DER verify key
+  const verifyKey = crypto.createPublicKey({
+    key: Buffer.from(KEYGEN_VERIFY_KEY, 'base64'),
+    format: 'der',
+    type: 'spki',
+  })
+
+  // Convert into bytes
+  const signatureBytes = Buffer.from(signature, 'base64')
+  const dataBytes = Buffer.from(data)
+
+  // Cryptographically verify data against the signature
+  const ok = crypto.verify(null, dataBytes, verifyKey, signatureBytes)
+  if (!ok) {
+    throw new Error(`Signature does not match: ${signature}`)
+  }
+}
+
 // Create a cache location based on the current timestamp down to the day,
 // so there will be a new location for each day. You may want to periodically
-// clean up old cache locations so this doesn't get out of hand.
+// clean up old cache locations so this doesn't get out of hand. Returns a
+// cache location.
 function getCurrentCacheLocation(key) {
   const dt = new Date().toDateString()
   const hash = crypto.createHash('sha1')
@@ -32,48 +87,57 @@ function getCurrentCacheLocation(key) {
   return `cache/${hash}`
 }
 
-function getCachedValidationResponse(key) {
+// Get a cached validation response. Returns the cache contents.
+async function getCachedValidationResponse(key) {
   const path = getCurrentCacheLocation(key)
-  if (fs.existsSync(path)) {
-    const contents = fs.readFileSync(path)
+
+  try {
+    const contents = await fs.readFile(path)
+    const data = JSON.parse(contents.toString())
+
     console.log(
       chalk.gray(`Cache hit: ${key} (${path})`)
     )
 
-    try {
-      const [sig, encRes] = contents.toString().split(':')
-      const res = Buffer.from(encRes, 'base64').toString()
-
-      return {
-        res: JSON.parse(res),
-        sig,
-      }
-    } catch (e) {
-      console.error(
-        chalk.red(`Cache err: ${e} (${key})`)
+    return data
+  } catch (e) {
+    // Cache location doesn't exist -- this is fine.
+    if (e.code === 'ENOENT') {
+      console.log(
+        chalk.gray(`Cache miss: ${key} (${path})`)
       )
 
-      fs.unlinkSync(path)
+      return null
     }
-  }
 
-  return null
+    console.error(
+      chalk.red(`Cache err: ${e} (${key})`)
+    )
+
+    await fs.unlink(path)
+  }
 }
 
-function setCachedValidationResponse(key, sig, res) {
+// Sets the validation response cache. Returns void.
+async function setCachedValidationResponse(key, { target, date, signature, body }) {
   const path = getCurrentCacheLocation(key)
+  const contents = JSON.stringify({
+    target,
+    date,
+    signature,
+    body,
+  })
+
   console.log(
     chalk.gray(`Cache set: ${key} (${path})`)
   )
 
-  const encRes = Buffer.from(JSON.stringify(res)).toString('base64')
-  const contents = `${sig}:${encRes}`
-
-  return fs.writeFileSync(path, contents)
+  await fs.writeFile(path, contents)
 }
 
-async function getValidationResponse(key) {
-  const res = await fetch(`https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/licenses/actions/validate-key`, {
+// Performs a license key validation request. Returns a response.
+async function performLicenseKeyValidation(key) {
+  return fetch(`https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/licenses/actions/validate-key`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/vnd.api+json',
@@ -83,72 +147,80 @@ async function getValidationResponse(key) {
       meta: { key }
     })
   })
-
-  return res.json().then(body => ({
-    sig: res.headers.get('X-Signature'),
-    res: body,
-  }))
 }
 
+// Validates a license key, using the cache. Returns the validation result
+// and license object.
 async function validateLicenseKey(key) {
   const cache = await getCachedValidationResponse(key)
-  if (cache) {
-    return cache
+  if (cache != null) {
+    const { target, date, signature, body } = cache
+
+    // Rehash the cached data
+    const sha256 = crypto.createHash('sha256').update(body)
+    const digest = `sha-256=${sha256.digest('base64')}`
+
+    // Verify the cached response
+    await verifyResponseSignature({ target, digest, date, signature })
+
+    return JSON.parse(body)
   }
 
-  const { res, sig } = await getValidationResponse(key)
-  const { meta, errors } = res
+  // Validate the license key
+  const res = await performLicenseKeyValidation(key)
+
+  // Get plaintext response body for signature verification
+  const body = await res.text()
+
+  // Parse the response body
+  const { meta, data, errors } = JSON.parse(body)
   if (errors) {
-    const msgs = errors.map(e => `${e.title}: ${e.detail}`)
-    console.error(
-      chalk.red(`Request err: ${msgs}`)
-    )
+    const msgs = errors.map(e => `${e.title}=${e.detail}`)
 
-    process.exit(1)
+    throw new Error(`API Error: ${msgs}`)
   }
 
+  // Check if license is valid (and cache the response if it is)
   if (meta.valid) {
-    setCachedValidationResponse(key, sig, res)
+    const { signature } = parseParameterizedHeader(res.headers.get('keygen-signature'))
+    const target = `post /v1/accounts/${KEYGEN_ACCOUNT_ID}/licenses/actions/validate-key`
+    const sha256 = crypto.createHash('sha256').update(body)
+    const digest = `sha-256=${sha256.digest('base64')}`
+    const date = res.headers.get('date')
+
+    // Verify the response
+    await verifyResponseSignature({ target, digest, date, signature })
+
+    // Cache the response
+    await setCachedValidationResponse(key, { target, date, signature, body })
   }
 
-  return { res, sig }
+  return { meta, data }
 }
 
-function verifyResponseSignature(res, sig) {
-  try {
-    const body = JSON.stringify(res)
-    const verifier = crypto.createVerify('sha256')
-    verifier.write(body)
-    verifier.end()
-
-    return verifier.verify(KEYGEN_PUBLIC_KEY, sig, 'base64')
-  } catch (e) {
-    return false
-  }
-}
-
+// Runs the main program. Exits with a non-zero code on failure.
 async function main() {
-  const key = await getLicenseKeyFromUser()
-  const { res, sig } = await validateLicenseKey(key)
-  const ok = verifyResponseSignature(res, sig)
-  if (!ok) {
-    console.error(
-      chalk.red(`Signature verification failed! Cached data has been tampered with.`)
+  try {
+    const key = await getLicenseKeyFromUser()
+    const { meta, data } = await validateLicenseKey(key)
+
+    if (meta.valid) {
+      console.log(
+        chalk.green(`License ${data.id} is valid!`)
+      )
+    } else {
+      console.log(
+        chalk.yellow(`License is not valid (${meta.detail})`)
+      )
+    }
+
+    process.exit(0)
+  } catch (e) {
+    console.log(
+      chalk.red(`Fatal: ${e.message}`)
     )
 
     process.exit(1)
-  }
-
-  const { meta, data } = res
-
-  if (meta.valid) {
-    console.log(
-      chalk.green(`License ${data.id} is valid!`)
-    )
-  } else {
-    console.log(
-      chalk.red(`License is not valid (${meta.detail})`)
-    )
   }
 }
 
